9c1d470ad460eb891c528a29c05ef70010bf2e69
SamirBB <61735486+SamirBB@users.noreply.github.com>
2026-04-06 23:46:55 +0200

    Initial Next.js app setup

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold">Salon app radi</h1>
+      <p className="mt-4">Next.js setup je uspješan.</p>
+    </main>
+  );
+}
